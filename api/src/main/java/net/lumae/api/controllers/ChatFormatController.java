641f042abf0ca13340c15550b8d69941434e0770
Samkist <samshark23451@gmail.com>
2022-01-07 22:00:48 -0500

    format-controllers

--- api/src/main/java/net/lumae/api/controllers/ChatFormatController.java
+++ api/src/main/java/net/lumae/api/controllers/ChatFormatController.java
@@ -1,8 +1,13 @@
 package net.lumae.api.controllers;
 
+import dev.samkist.lumae.sagittarius.data.models.ChatFormat;
 import net.lumae.api.repository.ChatFormatRepository;
+import net.lumae.api.repository.RecordNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SagittariusController
 public class ChatFormatController {
@@ -12,6 +17,31 @@ public class ChatFormatController {
 
     public ChatFormatController(ChatFormatRepository formats) {
         this.formats = formats;
+    }
+
+    @Cacheable("chatFormatCache")
+    @GetMapping("/formats/chat")
+    public List<ChatFormat> all() {
+        return formats.findAll();
+    }
+
+    @Cacheable("chatFormatCache")
+    @PostMapping("/formats/chat")
+    public ChatFormat newFormat(@RequestBody ChatFormat format) {
+        return formats.save(format);
+    }
+
+    @Cacheable("chatFormatCache")
+    @GetMapping("/formats/chat/{id}")
+    public ChatFormat one(@PathVariable String id) {
+        return formats.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id, ChatFormat.scope));
+    }
+
+    @Cacheable("chatFormatCache")
+    @DeleteMapping("/formats/chat/{id}")
+    public void delete(@PathVariable String id) {
+        formats.deleteById(id);
     }
 
 
---
+++ api/src/main/java/net/lumae/api/controllers/JoinLeaveFormatController.java
@@ -0,0 +1,49 @@
+package net.lumae.api.controllers;
+
+import dev.samkist.lumae.sagittarius.data.models.ChatFormat;
+import dev.samkist.lumae.sagittarius.data.models.JoinLeaveFormat;
+import net.lumae.api.repository.JoinLeaveFormatRepository;
+import net.lumae.api.repository.RecordNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SagittariusController
+public class JoinLeaveFormatController {
+
+    @Autowired
+    private final JoinLeaveFormatRepository formats;
+
+
+    public JoinLeaveFormatController(JoinLeaveFormatRepository formats) {
+        this.formats = formats;
+    }
+
+    @Cacheable("joinLeaveFormatCache")
+    @GetMapping("/formats/join-leave")
+    public List<JoinLeaveFormat> all() {
+        return formats.findAll();
+    }
+
+    @Cacheable("joinLeaveFormatCache")
+    @PostMapping("/formats/join-leave")
+    public JoinLeaveFormat newFormat(@RequestBody JoinLeaveFormat format) {
+        return formats.save(format);
+    }
+
+    @Cacheable("joinLeaveFormatCache")
+    @GetMapping("/formats/join-leave/{id}")
+    public JoinLeaveFormat one(@PathVariable String id) {
+        return formats.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id, JoinLeaveFormat.scope));
+    }
+
+    @Cacheable("joinLeaveFormatCache")
+    @DeleteMapping("/formats/join-leave/{id}")
+    public void delete(@PathVariable String id) {
+        formats.deleteById(id);
+    }
+
+}
